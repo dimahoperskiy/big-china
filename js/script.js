2ef2dda62886488b61c5c9bb99183a79ee420d93
dimitry <dimabymerang0@gmail.com>
2021-03-13 19:04:12 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -8,6 +8,14 @@
         $('.menuWrapper').toggleClass("_active");
     });
 
+    $('.menuLink').click(function(e){
+        e.preventDefault()
+        $('.ham').toggleClass("is-active");
+        $('.menu').toggleClass("_active");
+        $('.menuBg').toggleClass("_active");
+        $('.menuWrapper').toggleClass("_active");
+    });
+
     $('#spanOne').click(function(e){
         e.preventDefault()
         $('#questionOne').toggleClass("_active");
@@ -29,42 +37,17 @@
         $('.threePlus').toggleClass("_active");
     });
 
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
-                        };
-                    });
-                }
+    $('.smoothScroll').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000); // The number here represents the speed of the scroll in milliseconds
+                return false;
             }
-        });
-
-
+        }
+    });
 
 })(jQuery);
